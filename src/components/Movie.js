b11ac7e2a52049e25bf2ee54cedf36bc7e46fde5
MiMiMinGyu <awingmin@naver.com>
2025-02-11 22:08:35 +0900

    nomadcoder 영화 정보 사이트

---
+++ src/components/Movie.js
@@ -0,0 +1,33 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+function Movie({id, medium_cover_image, title, summary, genres}) {
+    return  (
+        <div>
+            <h2>
+              <Link to = {`/movie/${id}`}> {title} </Link>
+            </h2>
+            <img src = {medium_cover_image} alt = {title} />
+            <ul>
+              {genres.map(genre =>
+                <li key = {genre}>
+                  {genre}
+                </li>
+              )}
+            </ul>
+            <p>
+              {summary}
+            </p>
+        </div>
+    );
+}
+
+Movie.propTypes = {
+    id: PropTypes.number.isRequired,
+    medium_cover_image: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    summary: PropTypes.string.isRequired,
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired
+}
+
+export default Movie;
